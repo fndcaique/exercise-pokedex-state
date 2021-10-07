d7e2427dc7b6e66c81f9935ce08447147f9bb641
Fernando Soares <fndcaique@gmail.com>
2021-10-07 15:48:37 -0300

    add next and previus buttons

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,13 +2,34 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    render() {
-        return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
-        );
+  constructor() {
+    super();
+    this.state = {
+      pokemonIndex: 0,
     }
+  }
+
+  handleChangePokemon(incDec) {
+    this.setState((previusState, { pokemons }) => {
+      const len = pokemons.length;
+      const pokemonIndex = previusState.pokemonIndex + incDec >= len ? 0
+        : previusState.pokemonIndex + incDec < 0 ? len - 1
+          : previusState.pokemonIndex + incDec;
+      return {
+        pokemonIndex,
+      };
+    });
+  }
+  render() {
+    const pokemon = this.props.pokemons[this.state.pokemonIndex]
+    return (
+      <div className="pokedex">
+        <button onClick={() => this.handleChangePokemon(-1)}>Previus</button>
+        <Pokemon key={pokemon.id} pokemon={pokemon} />
+        <button onClick={() => this.handleChangePokemon(1)}>Next</button>
+      </div>
+    );
+  }
 }
 
 export default Pokedex;
